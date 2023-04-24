79eb568db37b1e46d2de4c70acf7d758d68932d6
Castro Hamiltone <castromiltone@gmail.com>
2023-04-24 23:04:43 +0300

    handles end-of-file (ctr+D) - _getline

--- _getline.c
+++ _getline.c
@@ -2,21 +2,28 @@
 
 /**
  * _getline - readline
+ * @lineptr: where to write the string
+ * @n: size of the string
+ * @stream: file descriptor
 */
 
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
   char *buff = malloc(sizeof(char *) * *n);
+  int k;
 
   if (buff == NULL)
     return (-1);
 
   if (stream != NULL)
-    read(STDIN_FILENO, buff, *n);
+    k = read(STDIN_FILENO, buff, *n);
 
   buff[str_len(buff)] = '\0';
 
   lineptr[0] = buff;
+
+  if (k  == 0)
+    return (-1);
   
-  return 1;
+  return str_len(buff);
 }
