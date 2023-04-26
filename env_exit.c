20c75efef90f3c8527eedf439513f090f60667a4
Congo Justus Migue <congomigue@gmail.com>
2023-04-26 14:47:31 +0000

    Solution to task 4 and 5

---
+++ env_exit.c
@@ -0,0 +1,130 @@
+#include "main.h"
+/**
+ * main - the main function of the simple shell program
+ *
+ * Return: 0 if success
+ */
+
+int main(void)
+{
+	char *buffer = NULL;
+	size_t buffer_size = 0;
+	ssize_t line_size;
+	char **args;
+
+	while (1)
+	{
+		printf("$ ");
+		line_size = getline(&buffer, &buffer_size, stdin);
+		if (line_size == -1)
+			break;
+		buffer[line_size - 1] = '\0';
+		args = tokenizer(buffer, " ");
+		if (args == NULL)
+			continue;
+		if (check_builtin(args) == 0)
+			execute(args);
+		free(args);
+	}
+	free(buffer);
+	return (0);
+}
+
+/**
+ * tokenizer - to tokenize the input buffer
+ * @str: input buffer
+ * @delimiter: delimiter character
+ *
+ * Return: array of tokens
+ */
+
+char **tokenizer(char *str, const char *delimiter)
+{
+	int i = 0;
+	char **tokens = NULL;
+	char *token = NULL;
+
+	if (str == NULL)
+		return (NULL);
+
+	tokens = malloc(sizeof(char *) * 1024);
+	if (tokens == NULL)
+		return (NULL);
+
+	token = strtok(str, delimiter);
+	while (token != NULL)
+	{
+		tokens[i] = token;
+		i++;
+		token = strtok(NULL, delimiter);
+	}
+	tokens[i] = NULL;
+	return (tokens);
+}
+
+/**
+ * execute - it executes the input command
+ * @args: input command and its arguments
+ *
+ * Return: void
+ */
+
+void execute(char **args)
+{
+	pid_t child_pid;
+	int status;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		perror("Fork error");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		execve(args[0], args, NULL);
+		perror("Command not found");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(child_pid, &status, WUNTRACED);
+	}
+}
+/**
+ * check_builtin - check if the input command is built-in
+ * @args: input the command and its arguments
+ *
+ * Return: 1 if the input command is built-in, else 0
+ */
+
+int check_builtin(char **args)
+{
+	if (strcmp(args[0], "exit") == 0)
+	{
+		exit(0);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		print_env();
+		return (1);
+	}
+	return (0);
+}
+
+/**
+ * print_env - to print the current environment
+ *
+ * Return: void
+ */
+
+void print_env(void)
+{
+	extern char **environ;
+	int i = 0;
+	while (environ[i] != NULL)
+	{
+		printf("%s\n", environ[i]);
+		i++;
+	}
+}
--- main.h
+++ main.h
@@ -1,48 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#define BUFFSIZE 1024
-
-/*
- * struct info_s - struct that contains potential argumnts for commands
- * @argv: Null-terminated array of strings containing arguments
- * passed to the command
- * @env: Null-terminated array of strings containing environment varibales.
- * @status: Exit status of the last command executed
- * @err_num: Integer value to represent error numbers
- */
-
-typedef struct info_s
-{
-	char **argv;
-	char **env;
-	int status;
-	int err_num;
-} info_t;
-
-
+/* Libraries */
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <stdio.h>
 
-
-void shell(char *args[], char *filename);
-char **prompt();
-char *teamstrncpy(char *dest, char *src, size_t n);
-char *teamstrncat(char *dest, char *src, int n);
-char *teamstrchr(char *s, char c);
-size_t _strlen(char *s);
-int _erratoi(char *new_str);
-int teamexit(info_t *info);
-int _eputchar(char new_char);
-void _eputs(char *new_str);
-void print_error(info_t *info, char *new_msg);
-int execute_command(info_t *info);
-char *read_line(int *should_free);
+/* Function prototypes*/
+char **tokenizer(char *str, const char *delimiter);
+void execute(char **args);
+int check_builtin(char **args);
+void print_env(void);
 
 #endif
