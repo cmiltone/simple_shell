8b814a62afbbf54b38ef5f629a094b29a9c07959
Castro Hamiltone <castromiltone@gmail.com>
2023-04-27 01:09:11 +0300

    env debug

--- shell.c
+++ shell.c
@@ -36,6 +36,18 @@ void shell(char *filename)
 	while (1)
 	{
 		cmd = prompt();
+
+		if (check_builtin(cmd) == 1)
+			continue;
+		else if (access(cmd[0], X_OK) != 0)
+		{
+			cmd[0] = get_command_in_path(cmd[0]);
+			if (cmd[0] == NULL)
+			{
+				perror(filename);
+				continue;
+			}
+		}
 		pid = fork();
 		if (pid > 0)
 			waitpid(pid, &status, 0);
