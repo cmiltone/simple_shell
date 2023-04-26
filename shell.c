12e76495d03bf6d40e60482b37eb6e2d4dc282a5
Castro Hamiltone <castromiltone@gmail.com>
2023-04-27 01:21:42 +0300

    debug

--- shell.c
+++ shell.c
@@ -37,9 +37,7 @@ void shell(char *filename)
 	{
 		cmd = prompt();
 
-		if (check_builtin(cmd) == 1)
-			continue;
-		else if (access(cmd[0], X_OK) != 0)
+		if (access(cmd[0], X_OK) != 0)
 		{
 			cmd[0] = get_command_in_path(cmd[0]);
 			if (cmd[0] == NULL)
